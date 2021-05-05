2820f5e3f2b0f931de8965ec3e5e7767d7578e0d
SebastianMestre <mestresebastianunr@gmail.com>
2021-05-05 17:17:10 -0300

    adapt interpreter to handle different data types

--- src/interpreter.hpp
+++ src/interpreter.hpp
@@ -3,27 +3,90 @@
 #include "frontend.hpp"
 
 #include <cassert>
+#include <iostream>
+
+struct Value {
+	enum struct Tag {
+		Int,
+		Num,
+	};
+	Tag tag;
+	union {
+		int as_int;
+		float as_num;
+	};
+};
+
+Value make_value(int int_value) {
+	Value result;
+	result.tag = Value::Tag::Int;
+	result.as_int = int_value;
+	return result;
+}
+
+Value make_value(float num_value) {
+	Value result;
+	result.tag = Value::Tag::Num;
+	result.as_num = num_value;
+	return result;
+}
+
+Value operator+ (Value lhs, Value rhs) {
+	assert(lhs.tag == rhs.tag);
+	switch (lhs.tag) {
+	case Value::Tag::Int:
+		return make_value(lhs.as_int + rhs.as_int);
+	case Value::Tag::Num:
+		return make_value(lhs.as_num + rhs.as_num);
+	}
+}
+
+std::ostream& operator<< (std::ostream& o, Value value) {
+	switch (value.tag) {
+	case Value::Tag::Int:
+		return o << value.as_int;
+	case Value::Tag::Num:
+		return o << value.as_num;
+	}
+}
 
 struct Interpreter {
-	std::unordered_map<String::Id, int> vars;
-	std::vector<int> stack;
+	std::unordered_map<String::Id, Value> vars;
+	std::vector<Value> stack;
 
-	int get(String::Id id) {
+	Value get(String::Id id) {
 		auto it = vars.find(id);
 		assert(it != vars.end());
 		return it->second;
 	}
 
-	void set(String::Id id, int value) {
+
+	void set(String::Id id, Value value) {
 		vars[id] = value;
 	}
 
-	void push(int value) {
+	void set(String::Id id, int int_value) {
+		set(id, make_value(int_value));
+	}
+
+	void set(String::Id id, float num_value) {
+		set(id, make_value(num_value));
+	}
+
+	void push(Value value) {
 		stack.push_back(value);
 	}
 
-	int pop() {
-		int result = stack.back();
+	void push(int int_value) {
+		push(make_value(int_value));
+	}
+
+	void push(float num_value) {
+		push(make_value(num_value));
+	}
+
+	Value pop() {
+		Value result = stack.back();
 		stack.pop_back();
 		return result;
 	}
@@ -37,8 +100,8 @@ struct Interpreter {
 				set(ast.as_decl.name, pop());
 				break;
 			case Ast::Tag::Sum: {
-				int lhs = pop();
-				int rhs = pop();
+				Value rhs = pop();
+				Value lhs = pop();
 				push(lhs + rhs);
 				} break;
 			case Ast::Tag::Int:
